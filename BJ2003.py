04312fea255167073232c3bcfb78a1821ca33885
salduf <04fud27@gmail.com>
2021-01-31 15:35:01 +0900

    BaekJoon_TwoPointer

---
+++ BJ2003.py
@@ -0,0 +1,30 @@
+# 21.01.31 [수들의 합 2]
+
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+a.append(0)
+
+result = 0
+i = 0
+j = 0
+now = a[i]
+while j < n:
+	if now == m:
+		result+=1
+		now -= a[i]
+		i +=1
+		j +=1
+		now += a[j]
+	elif now > m:
+		now -= a[i]
+		i+=1
+		if i > j :
+			j = i
+			now = a[i]
+	else:
+		j+=1
+		now += a[j]
+
+print(result)
