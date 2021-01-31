eac53b01f9098601f276006cb97e309370d0a4a4
salduf <04fud27@gmail.com>
2021-01-31 15:45:17 +0900

    BaekJoon_Sort

---
+++ BJ1449.py
@@ -0,0 +1,15 @@
+# 21.01.31 [수리공 항승]
+import sys
+n, l = map(int, sys.stdin.readline().split())
+spot = list(map(int, sys.stdin.readline().split()))
+
+spot.sort()
+
+now = spot.pop()
+result =1
+while spot:
+	temp = spot.pop()
+	if now-temp >= l:
+		now = temp
+		result+=1
+print(result)
