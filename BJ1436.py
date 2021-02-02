c74f4be459e9d037a536c16827d9542176901d88
salduf <04fud27@gmail.com>
2021-02-02 15:52:24 +0900

    BaekJoon_DFS/BFS

--- BJ1436.py
+++ BJ1436.py
@@ -1,12 +1,26 @@
-# 21.02.01 [영화감독 숌]
+# 21.02.02 [트리]
 
 import sys
-n = int(sys.stdin.readline())
+from collections import defaultdict
 
-now=1
-result = 666
-while n > now:
-	result+=1
-	if '666' in str(result):
-		now+=1
+def dfs(now):
+	global result
+	if not dic[now]:
+		result+=1
+		return
+	for i in dic[now]:
+		dfs(i)
+
+n = int(sys.stdin.readline())
+node = list(map(int, sys.stdin.readline().split()))
+delete = int(sys.stdin.readline())
+dic = defaultdict(list)
+for i in range(n):
+	if i == delete or node[i] == delete:
+		continue
+	dic[node[i]].append(i)
+
+result=0
+if dic[-1]:
+	dfs(-1)
 print(result)
