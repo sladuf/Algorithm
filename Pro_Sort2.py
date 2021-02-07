6906c9f2ce06d72f89f1292d06f45f57f34519a7
salduf <04fud27@gmail.com>
2021-02-07 23:28:40 +0900

    Programmers_Sort

---
+++ Pro_Sort2.py
@@ -0,0 +1,13 @@
+# 21.02.07 [가장 큰 수]
+def solution(numbers):
+    answer = ''
+    numbers.sort(key = lambda x: str(x)*3, reverse=True)
+    '''
+    최대 1000이므로 자릿수를 맞추기 위해 *3
+    98 vs 9 -> 989898 vs 999 문자열 비교이므로 8 < 9 라서 9가 98보다 앞으로 정렬됨
+    '''
+    if numbers[0] == 0:
+        return "0"
+    for i in numbers:
+        answer+=str(i)
+    return answer
---
+++ Pro_Sort3.py
@@ -0,0 +1,16 @@
+# 21.02.07 [H-Index]
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    h=0
+    #i가 문제의 h의 역할을, h는 h번 이상 인용된 논문의 갯수임
+    for i in range(len(citations),-1,-1):
+        #h는 논문의 갯수보다 클 수 없음
+        while h < len(citations):
+            if citations[h] >= i:
+                h+=1
+            else:
+                break
+        if h>=i:
+            answer = i
+            return answer
