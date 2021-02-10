7a2dec6bb5a1ab51a6d9bf8a82421c3de738644d
salduf <04fud27@gmail.com>
2021-02-11 03:43:06 +0900

    Programmers_Sort

---
+++ PRO42748.py
@@ -0,0 +1,9 @@
+# 21.02.10 [K번째수]
+def solution(array, commands):
+    answer = []
+    for a,b,c in commands:
+        temp = array[a-1:b]
+        temp.sort()
+        answer.append(temp[c-1])
+        
+    return answer
