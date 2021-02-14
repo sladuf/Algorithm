0e92912b63fa84913a7dafd19550ba3900e18575
salduf <04fud27@gmail.com>
2021-02-14 16:49:57 +0900

    Programmers_Greedy

---
+++ PRO42861.py
@@ -0,0 +1,17 @@
+# 21.02.14 [섬 연결하기]
+def solution(n, costs):
+    answer = 0
+    costs.sort(key=lambda x: x[2])
+    #방문 node 체크 하는 집합
+    node = set([costs[0][0]])
+    
+    while len(node)<n:
+        for a,b,c in costs:
+            if a in node and b in node :
+                continue
+            elif a in node or b in nogiotde:
+                node.update([a,b])
+                answer += c
+                break
+        
+    return answer
---
+++ PRO42884.py
@@ -0,0 +1,11 @@
+# 21.02.14 [단속카메라]
+def solution(routes):
+    routes.sort()
+    now=routes[-1][0]
+    answer=1
+    for i in range(len(routes)-2,-1,-1):
+        if routes[i][1] < now:
+            now=routes[i][0]
+            answer+=1
+            
+    return answer
