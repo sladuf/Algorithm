42804c0026968d24e29c86d9f3e67eb7a9dd47f5
salduf <04fud27@gmail.com>
2021-02-15 22:05:41 +0900

    BaekJoon_Stack

---
+++ BJ17298.py
@@ -0,0 +1,23 @@
+# 21.02.15 [오큰수] / Stack
+import sys
+
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+nge = [-1 for x in range(n)]
+
+stack = []
+for i in range(n-1, -1, -1):
+    while stack:
+        if stack[-1] > a[i]:
+            nge[i] = stack[-1]
+            stack.append(a[i])
+            break
+        else:
+            stack.pop()
+
+    if not stack:
+        stack.append(a[i])
+
+for i in nge:
+    print(i, end=' ')
+
---
+++ BJ1874.py
@@ -0,0 +1,36 @@
+# 21.02.15 [스택 수열] / Stack
+import sys
+
+n = int(sys.stdin.readline())
+
+s = []
+for j in range(n):
+	s.append(int(sys.stdin.readline()))
+
+result = ['+']
+stack = [1]
+i = 0
+num = 2
+while num <= n :
+	if not stack or stack[-1] != s[i]:
+		stack.append(num)
+		result.append('+')
+		num+=1
+		continue
+
+	stack.pop()
+	result.append('-')
+	i+=1
+
+while stack:
+	if stack[-1] == s[i]:
+		result.append('-')
+		i+=1
+		stack.pop()
+	else:
+		print('NO')
+		break
+
+if len(stack) == 0:
+	for i in result:
+		print(i)
