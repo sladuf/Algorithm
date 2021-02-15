df6e50627cd63738430d954c1a14db466306569c
salduf <04fud27@gmail.com>
2021-02-15 22:05:13 +0900

    BaekJoon_Greedy

---
+++ BJ13305.py
@@ -0,0 +1,14 @@
+# 21.02.15 [주유소] / Greedy
+import sys
+
+n = int(sys.stdin.readline())
+length = list(map(int,sys.stdin.readline().split()))
+cost = list(map(int, sys.stdin.readline().split()))
+
+now = cost[0]
+result = 0
+for i in range(len(length)):
+    if now > cost[i]:
+        now = cost[i]
+    result+=(now*length[i])
+print(result)
