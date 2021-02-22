fbf7ee8cef197b1320529f135f657496bc4e25e7
salduf <04fud27@gmail.com>
2021-02-22 16:49:26 +0900

    BaekJoon_DFS/BFS

--- BJ2206.py
+++ BJ2206.py
@@ -1,25 +1,35 @@
 # 21.01.27 [벽 부수고 이동하기]
+import sys
 from collections import deque
 
-dx, dy = [0, -1, 0, 1], [-1, 0, 1, 0]
-def sol():
-    ck = [[[0]*2 for _ in range(M)] for _ in range(N)]
-    ck[0][0][0] = 1
-    while q:
-        x, y, wall = q.popleft()
-        if x==(N-1) and y==(M-1): return ck[x][y][wall]
-        for i in range(4):
-            if 0 <= x+dx[i] < N and 0 <= y+dy[i] < M and ck[x+dx[i]][y+dy[i]][wall] == 0:
-                if Map[x+dx[i]][y+dy[i]]=='0':
-                    ck[x+dx[i]][y+dy[i]][wall] = ck[x][y][wall] + 1
-                    q.append([x+dx[i], y+dy[i], wall])
-                if wall == 0 and Map[x+dx[i]][y+dy[i]] == '1':
-                    ck[x+dx[i]][y+dy[i]][1] = ck[x][y][0] + 1
-                    q.append([x+dx[i], y+dy[i], 1])
-    return -1
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
 
-N, M = map(int, input().split())
-Map = [list(input()) for _ in range(N)]
-q = deque()
-q.append([0, 0, 0])
-print(sol())
+def bfs():
+    wall[0][0][0] = 1
+    queue = deque([[0,0,0]])
+    while queue:
+        x, y, z = queue.popleft()
+        if x ==(n-1) and y==(m-1) : return wall[x][y][z]
+        for i in range(4):
+            xx=dx[i]+x
+            yy=dy[i]+y
+            if 0<=xx<n and 0<=yy<m and wall[xx][yy][z] == 0:
+                if mapp[xx][yy] == 0:
+                    wall[xx][yy][z] = wall[x][y][z]+1
+                    queue.append([xx,yy,z])
+                elif z==0:
+                    wall[xx][yy][1] = wall[x][y][0]+1
+                    queue.append([xx,yy,1])
+
+n,m = map(int, sys.stdin.readline().split())
+
+mapp = []
+for i in range(n):
+    temp = list(sys.stdin.readline().strip())
+    mapp.append(list(map(int, temp)))
+
+wall = [[[0]*2 for x in range(m)] for x in range(n)]
+result = bfs()
+if result: print(result)
+else: print(-1)
