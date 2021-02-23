fb9e6a6912b4cc805d0a8b83b95d246689b2d6c7
salduf <04fud27@gmail.com>
2021-02-23 14:59:49 +0900

    BaekJoon_BT

--- BJ2110.py
+++ BJ2110.py
@@ -1,29 +1,30 @@
-# 21.02.19 [공유기 설치]
+# 21.02.23 [좋은 수열]
 import sys
-n,c = map(int, sys.stdin.readline().split())
-x = []
-for i in range(n):
-    x.append(int(sys.stdin.readline()))
-x.sort()
 
-if c == 2:
-    print(x[-1]-x[0])
-else:
-    c-=2
-    l = 1
-    r = x[-1]
-    while l <= r:
-        mid = (l+r)//2
-        pre = x[0]
-        s = 0
-        for i in range(1,n-1):
-            if x[i]-pre >= mid and x[-1]-x[i] >= mid:
-                s+=1
-                pre = x[i]
-            if s > c:
-                break
-        if s>=c:
-            l = mid+1
-        else:
-            r = mid-1
-    print(r)
+def ch(cnt):
+    #좋은 수열인지 확인하기
+    for i in range(1,cnt//2+1):
+        if res[cnt-i:cnt] == res[cnt-(2*i):cnt-i]:
+            return False
+
+    #좋은 수열인데 길이가 n인지 확인
+    if cnt == n:
+        for i in res:
+            print(i, end='')
+        return True
+
+    #아직 길이가 n이 아니라면 1,2,3 추가
+    for i in range(1,4):
+        #연속된 숫자 X
+        if res[-1] == i:
+            continue
+
+        #i를 넣어보고 좋은 수열이 아니면 pop
+        res.append(i)
+        if ch(cnt+1):
+            return True
+        res.pop()
+
+n = int(sys.stdin.readline())
+res = [1]
+ch(1)
