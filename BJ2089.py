239748f7b521c50d21f1c44795aa15ae60af0301
salduf <04fud27@gmail.com>
2021-03-04 00:21:22 +0900

    BaekJoon

---
+++ BJ2089.py
@@ -0,0 +1,18 @@
+# 21.03.03 [-2진수]
+import sys
+
+n = int(sys.stdin.readline())
+
+if n == 0:
+    print(0)
+else:
+    res = []
+    while n != 0:
+        if n%(-2) == 0:
+            res.append('0')
+            n = n//(-2)
+        else:
+            res.append('1')
+            n = n//(-2) +1
+
+    print(''.join(reversed(res)))
