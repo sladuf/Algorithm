8e58f657c66f53119248223b8160889b285624c0
salduf <04fud27@gmail.com>
2021-03-05 02:44:45 +0900

    BaekJoon_DFS/BFS

--- BJ1202.py
+++ BJ1202.py
@@ -1,35 +1,71 @@
-# 21.02.25 [보석 도둑]
+# 21.03.04 [다리 만들기]
 '''
-보석과 가방을 무게 순서대로 정렬
-보석의 무게가 가방보다 가볍거나 같은거 다 heap정렬(가격기준으로 내림차순)
-정렬이 끝나면 그 중에 젤 가격 높은거 pop
-다음 가방 무게랑 비교해서 가방 갯수만큼 계속 반복
+스스로 풀었다는 것에 의의를 두는 엉망진창 코드...
+pypy로 간신히..통과했다!
+섬에 각자 고유번호를 매겨주고 섬 하나마다 다른 섬으로 가는 경우를 bfs한다.
 '''
 import sys
-import heapq
+from collections import deque
+import copy
 
-n,k = map(int, sys.stdin.readline().split())
-gold = []
-bag = []
+dx=[0,0,1,-1]
+dy=[1,-1,0,0]
+
+#land는 섬의 고유번호 해당 섬이 다른 섬을 만나는 경우를 check
+#섬이 바다를 만나는 경우 바다를 land로 바꿔주고 경로 +1 한다.
+#다른 섬을 만나면 경로의 횟수를 최소와 비교하고 return
+def bfs(land,queue):
+	global res
+
+	cp = copy.deepcopy(t)
+	while queue:
+		x,y,cnt = queue.popleft()
+		for i in range(4):
+			xx=x+dx[i]
+			yy=y+dy[i]
+			if 0<=xx<n and 0<=yy<n :
+				if cp[xx][yy] == 0:
+					cp[xx][yy] = land
+					queue.append([xx,yy,cnt+1])
+				elif cp[xx][yy] != land:
+					res=min(res,cnt)
+					return
+
+	return land 
+
+#섬에 고유번호를 매겨주는 함수
+#동시에 queue에 해당 섬을 넣는다.
+def ch(x,y,land):
+	q = deque([[x,y]])
+	t[x][y] = land
+	queue = deque([[x,y,0]])
+	while q:
+		x,y = q.popleft()
+		for i in range(4):
+			xx=dx[i]+x
+			yy=dy[i]+y
+			if 0<=xx<n and 0<=yy<n and t[xx][yy]==1:
+				q.append([xx,yy])
+				t[xx][yy]=land
+				queue.append([xx,yy,0])
+	return queue
+
+
+n=int(sys.stdin.readline())
+t = []
 for i in range(n):
-	a,b = map(int, sys.stdin.readline().split())
-	heapq.heappush(gold,[a,b])
-for i in range(k):
-	a = int(sys.stdin.readline())
-	bag.append(a)
+	t.append(list(map(int, sys.stdin.readline().split())))
 
-bag.sort()
+res = n*n
+land=2
+for i in range(n):
+	for j in range(n):
+		if t[i][j] == 1:
+			#섬이면 고유번호를 붙인 뒤에 bfs한당
+			queue = ch(i,j,land)
+			bfs(land,queue)
+			land+=1
 
-# w는 가격 기준으로 내림차순 하는 heap
-w = []
-res = 0
-for i in bag:
-	while gold:
-		if gold[0][0] <= i:
-			a,b = heapq.heappop(gold)
-			heapq.heappush(w,-b)
-		else:
-			break
-	if w:
-		res+= -1*heapq.heappop(w)
 print(res)
+
+
