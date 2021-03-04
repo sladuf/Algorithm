c3a402a1c49b715e257fd108bfb34e0481b5b7ae
salduf <04fud27@gmail.com>
2021-03-04 20:35:09 +0900

    BaekJoon

---
+++ BJ2331.py
@@ -0,0 +1,18 @@
+# 21.03.04 [반복수열]
+import sys
+
+a,b = map(int,sys.stdin.readline().split())
+res = [a]
+
+while True:
+	num = 0
+	for i in str(res[-1]):
+		num+=int(i)**b
+	if num in res:
+		t = res.index(num)
+		res = res[:t]
+		break
+	else:
+		res.append(num)
+
+print(len(res))
