2fdeca5188bd65d9ff6d808582ce3247dcfbbf58
sladuf <04fud27@gmail.com>
2022-01-31 02:39:21 +0900

    최소 힙

---
+++ BaekJoon/BJ1927.py
@@ -0,0 +1,17 @@
+# 22.01.31 [최소 힙]
+
+import sys, heapq
+
+n = int(sys.stdin.readline())
+
+heap = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, x)
