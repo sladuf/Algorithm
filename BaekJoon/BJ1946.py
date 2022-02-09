7a9f3b744c5258c8ab7829fbd24437893190ef66
sladuf <04fud27@gmail.com>
2022-02-09 17:58:44 +0900

    신입 사원

--- BaekJoon/BJ1946.py
+++ BaekJoon/BJ1946.py
@@ -1,20 +1,20 @@
-# 21.01.30 [신입 사원]
+# 22.02.09 [신입 사원]
+
 import sys
 
 t = int(sys.stdin.readline())
 
 for i in range(t):
 	n = int(sys.stdin.readline())
-	score = []
+	arr = []
 	for j in range(n):
 		a,b = map(int, sys.stdin.readline().split())
-		score.append([a,b])
-		
-	result = 1
-	score.sort()
-	b = score[0][1]
-	for j in range(1, len(score)) :
-		if score[j][1] < b:
-			b = score[j][1]
-			result+=1
-	print(result)
+		arr.append([a,b])
+	arr.sort()
+	res = 1
+	mx = arr[0][1]
+	for i in range(n):
+		if mx > arr[i][1]:
+			res+=1
+			mx = arr[i][1]
+	print(res)
