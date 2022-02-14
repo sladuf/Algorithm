89f580ef35f71c7a39cc4df9586a40b749ba2f5b
sladuf <04fud27@gmail.com>
2022-02-15 00:37:52 +0900

    2xn 타일링

--- BaekJoon/BJ11726.py
+++ BaekJoon/BJ11726.py
@@ -1,13 +1,18 @@
-# 21.01.28 [2xN 타일링]
+# 22.02.15 [2xN 타일링]
 
 import sys
 
 n = int(sys.stdin.readline())
-
-dp = [0 for i in range(n+1)]
+'''
+2x1 : 세로1
+2x2 : 세로1+세로1, 가로2
+(가로2는 한쌍으로 움직여야해)
+2x3 : 2x1 + 가로2, 2x2 + 세로1
+'''
+dp = [0] * (n+1)
 dp[0] = 1
 dp[1] = 1
 for i in range(2, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+    dp[i] += (dp[i-1] + dp[i-2])%10007
 
-print(dp[n]%10007)
+print(dp[n])
