3b8dbb3b4e0d615062c3d8d934f44a68fc08ea68
sladuf <04fud27@gmail.com>
2022-02-16 02:07:42 +0900

    GRB거리

--- BaekJoon/BJ1149.py
+++ BaekJoon/BJ1149.py
@@ -1,15 +1,19 @@
-# 21.01.29 [GRB거리]
+# 22.02.16 [GRB거리]
+
+'''
+위 값의 최소 + 내꺼
+'''
 
 import sys
-n = int(sys.stdin.readline())
-cost = []
-for i in range(n):
-	temp = list(map(int, sys.stdin.readline().split()))
-	cost.append(temp)
 
-for i in range(1, n):
-	cost[i][0] += min(cost[i-1][1], cost[i-1][2])
-	cost[i][1] += min(cost[i-1][0], cost[i-1][2])
-	cost[i][2] += min(cost[i-1][0], cost[i-1][1])
+n=int(sys.stdin.readline())
 
-print(min(cost[n-1]))
+rgb = [[0,0,0]]
+for i in range(1, n+1):
+	r,g,b = map(int, sys.stdin.readline().split())
+	rgb.append([r,g,b])
+	rgb[i][0] += min([rgb[i-1][1],rgb[i-1][2]])
+	rgb[i][1] += min([rgb[i-1][0],rgb[i-1][2]])
+	rgb[i][2] += min([rgb[i-1][0],rgb[i-1][1]])
+
+print(min(rgb[n]))
