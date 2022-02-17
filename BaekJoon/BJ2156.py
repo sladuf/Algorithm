02731e67478d4e76ac9c261fb533c42bc1973868
sladuf <04fud27@gmail.com>
2022-02-17 21:18:29 +0900

    포도주 시식

--- BaekJoon/BJ2156.py
+++ BaekJoon/BJ2156.py
@@ -1,16 +1,28 @@
-# 21.02.13 [포도주 시식]
+# 22.02.17 [포도주 시식]
+'''
+f(0) = 0
+f(1) = i
+f(2) = i+(i-1)
+
+f(i) = max(i+i-1+f(i-3) , i+f(i-2) , f(i-1))
+'''
 import sys
 
 n = int(sys.stdin.readline())
-
-p = [0]
+juice = [0]
+res = [0] * (n+1)
 for i in range(n):
-    p.append(int(sys.stdin.readline()))
-if n == 1:
-    print(p[1])
-else:
-    dp = [0,p[1],p[1]+p[2]]
-    for i in range(3, n+1):
-        dp.append(max(dp[i-3]+p[i]+p[i-1], dp[i-2]+p[i], dp[i-1]))
+    juice.append(int(sys.stdin.readline()))
 
-    print(dp[-1])
+res[1] = juice[1]
+if n>=2:
+    res[2] = juice[2]+juice[1]
+
+for i in range(3,n+1):
+    temp1 = juice[i]+juice[i-1]+res[i-3]
+    temp2 = juice[i]+res[i-2]
+    temp3 = res[i-1]
+    maxi = max([temp1, temp2, temp3])
+    res[i] = maxi
+
+print(res[n])
