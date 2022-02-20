9068554395fab933483e8774c9362c4e81a0c0ef
sladuf <04fud27@gmail.com>
2022-02-20 17:25:05 +0900

    오르막 수

--- BaekJoon/BJ11057.py
+++ BaekJoon/BJ11057.py
@@ -1,14 +1,15 @@
-# 21.01.28 [오르막 수]
+# 22.02.20 [오르막 수]
+
 '''
-10844 [쉬운 계단 수] 풀고나서 풀어서 쉽게 풀 수 있었당 (운빨이란 소리)
+1 1 1 1 1 1 1 1 1 1
+10 9 8 7 6 5 4 3 2 1
 '''
 import sys
 
 n = int(sys.stdin.readline())
-dp = [[1]*10 for y in range(n+1)]
+dp=[[1]*10 for i in range(n+1)]
 for i in range(2, n+1):
-	for j in range(1, 10):
-		dp[i][j] = dp[i-1][j] + dp[i][j-1]
+	for j in range(8,-1,-1):
+		dp[i][j] = dp[i][j+1] + dp[i-1][j]
 
-result = sum(dp[n])
-print(result%10007)
+print(sum(dp[n])%10007)
